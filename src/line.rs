ac3e102ede74f275dac2577fe023b5c506417430
Derek <physci@gmail.com>
2020-02-01 14:19:49 -0500

    reorganizes source

---
+++ src/line.rs
@@ -0,0 +1,53 @@
+use crate::types::*;
+use serde_json::Value;
+// use std::error::Error;
+
+// type E<T> = Result<T, Box<dyn Error>>;
+
+// fn err<T>(msg: &str) -> Result<T, Box<dyn Error>> {
+//     Err(From::from(msg.to_string()))
+// }
+
+impl Line {
+    pub fn from_value(val: Value) -> E<Line> {
+        match val {
+            Value::Array(v) => {
+                if v.len() != 2 {
+                    return err("line expects array with two args");
+                }
+                if v[0] != Value::String("line".to_string()) {
+                    return err("Line expects first array element to be the string 'line'");
+                }
+                let c: Coord5 = serde_json::from_value(v[1].clone())?;
+                Ok(Line { coord: c })
+            }
+            _ => err("line expects array with two args"),
+        }
+    }
+}
+
+// -----------------------------------------------------------------------------
+// TESTS
+// -----------------------------------------------------------------------------
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    //use crate::types::*;
+
+    #[test]
+    fn coord5() {
+        let expect = Coord5 { x: 1, y: 2, r: Rot::Rot270, dx: 3, dy: 4 };
+        let got: Coord5 = serde_json::from_str(&"[1, 2, 1, 3, 4]").unwrap();
+        assert_eq!(expect, got);
+    }
+
+    #[test]
+    fn line1() {
+        let expect = Line { coord: Coord5 { x: 1, y: 2, r: Rot::Rot270, dx: 3, dy: 4 } };
+        let s = r#"["line", [1, 2, 1, 3, 4]]"#;
+        let v: Value = serde_json::from_str(s).unwrap();
+        let got: Line = Line::from_value(v).unwrap();
+        assert_eq!(expect, got);
+    }
+}
---
+++ src/types.rs
@@ -0,0 +1,70 @@
+use serde::{Deserialize, Serialize};
+use serde_repr::*;
+use std::error::Error;
+
+pub type E<T> = Result<T, Box<dyn Error>>;
+
+pub fn err<T>(msg: &str) -> Result<T, Box<dyn Error>> {
+    Err(From::from(msg.to_string()))
+}
+
+// -----------------------------------------------------------------------------
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+pub struct Coord5 {
+    pub x: u32,
+    pub y: u32,
+    pub r: Rot,
+    pub dx: u32,
+    pub dy: u32,
+}
+
+// -----------------------------------------------------------------------------
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+pub struct Coord3 {
+    pub x: u32,
+    pub y: u32,
+    pub r: Rot,
+}
+
+// ----------------------------------------------------------------------------
+#[derive(Debug, PartialEq)]
+pub struct Line {
+    pub coord: Coord5,
+}
+
+// -----------------------------------------------------------------------------
+#[derive(Serialize_repr, Deserialize_repr, PartialEq, Debug)]
+#[repr(u8)]
+pub enum Rot {
+    Rot0 = 0,
+    Rot270 = 1,
+    Rot180 = 2,
+    Rot90 = 3,
+    FlipX = 4,
+    TransposeNeg = 5,
+    FlipY = 6,
+    TransposePos = 7,
+}
+pub use Rot::*;
+
+// -----------------------------------------------------------------------------
+#[derive(Serialize_repr, Deserialize_repr, PartialEq, Debug)]
+#[repr(u8)]
+enum Direction {
+    In = 0,
+    Out = 1,
+    InOut = 2,
+}
+
+// ----------------------------------------------------------------------------
+
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+enum Sig {
+    SigSimple(String),
+    SigIndex(String, i32),
+    SigHash(String, i32),
+    SigRange(String, i32, i32),
+    SigRangeStep(String, i32, i32, i32),
+    SigQuote(i32, i32),
+    SigConcat(Vec<Box<Sig>>),
+}
