1f43e107e6e1fd7daa448f5a7b1ebfe4b3dd60e6
Derek <physci@gmail.com>
2020-02-05 15:45:03 -0500

    return an interator into Value because tagged arrays may have more than one element

--- src/common.rs
+++ src/common.rs
@@ -1,14 +1,15 @@
 use crate::types::*;
 use serde_json::Value;
+use std::slice::Iter;
 
-pub fn tagged_array(tag: &str, val: &Value) -> E<Value> {
+pub fn tagged_array<'a>(tag: &str, val: &'a Value) -> E<Iter<'a, Value>> {
     if let Value::Array(v) = val {
         if v.len() != 2 {
             bail!(format!("{} tagged array expects array with two args", tag).as_str())
         } else if v[0] != Value::String(tag.to_string()) {
             bail!(format!("{} tagged array expects first array element to be the string {}", tag, tag).as_str())
         } else {
-            Ok(v[1].clone())
+            Ok(v[1..].iter())
         }
     } else {
         bail!(format!("{} tagged array expects array with two args", tag).as_str())
